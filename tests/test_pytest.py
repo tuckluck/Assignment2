fe1dba0e70c7a1649f935feaabab8ed4283c31eb
tuckluck <tsanderslyle@gmail.com>
2025-03-07 20:52:06 -0500

    Update test_pytest.py

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,68 +1,132 @@
 import pytest
 import numpy as np
 import math as mt
-from direct_stiffness import (
-    global_stiffness_mat, solve_for_displacements_and_reactions, compute_internal_forces,
-    global_3D_stiffness_mat, solve_for_Kg_global_ff, solve_gen_eig
-)
+from Scripts import direct_stiffness as ds
+
+node_cordinates = np.matrix([[0,0,0],
+                            [10,0,0],
+                            [10,20,0],
+                            [0,20,0],
+                            [0,0,25],
+                            [10,0,25],
+                            [10,20,25],
+                            [0,20,25]])
+
+#enter nodal forces/torques for each node as a row (Fx, Fy, Fx, Mx, My, Mz)
+#the first row will be considered node 1 and so on
+node_force = np.matrix([[0,0,0,0,0,0],
+                        [0,0,0,0,0,0],
+                       [0,0,0,0,0,0],
+                       [0,0,0,0,0,0],
+                       [0,0,-1,0,0,0],
+                       [0,0,-1,0,0,0],
+                       [0,0,-1,0,0,0],
+                       [0,0,-1,0,0,0]])
+
+#enter nodal degrees of freedom for each node as a row (x,y,z,rot_x,rot_y,rot_z)
+#0 for constrained and 1 for free
+#the first row will be considered node 1 and so on
+node_DOF = np.matrix([[0,0,0,0,0,0],
+                      [0,0,0,0,0,0],
+                      [0,0,0,0,0,0],
+                      [0,0,0,0,0,0],
+                      [1,1,1,1,1,1],
+                     [1,1,1,1,1,1],
+                     [1,1,1,1,1,1],
+                     [1,1,1,1,1,1]])
+
+member_localz = np.array([0,0,0,0,0,0,0,0], dtype=object)
+
+
+#enter member properties and connecting nodes E, nu, A, Iy, Iz, J, 1, 2
+#the first row will be considered member A, the second row will be member B and so on
+r = .5
+E = 500
+nu = .3
+A = np.pi*r**2
+Iy = (np.pi*r**4)/4
+Iz = (np.pi*r**4)/4
+Irho = (np.pi*r**4)/2
+J = (np.pi*r**4)/2
+
+
+
+
+
+members = np.matrix([[E, nu, A, Iy, Iz, Irho, J, 1, 5],    #member E0
+                     [E, nu, A, Iy, Iz, Irho, J, 2, 6],    #member E1
+                     [E, nu, A, Iy, Iz, Irho, J, 3, 7],    #member E2
+                     [E, nu, A, Iy, Iz, Irho, J, 4, 8],    #member E3
+                     [E, nu, A, Iy, Iz, Irho, J, 5, 6],     #member E4
+                     [E, nu, A, Iy, Iz, Irho, J, 6, 7],     #member E5
+                     [E, nu, A, Iy, Iz, Irho, J, 7, 8],     #member E6
+                     [E, nu, A, Iy, Iz, Irho, J, 8, 5]])     #member E7
+
+
+
+F_global = np.zeros((len(node_DOF)*6, 1))  # 24x1 zero matrix    
+DOF_global = np.zeros((len(node_DOF)*6, 1))  # 24x1 zero matrix
+
+for i in range(len(node_force)):  
+    F_global[i*6:i*6+6, 0] = node_force[i].flatten()
+
+for i in range(len(node_DOF)):  
+    DOF_global[i*6:i*6+6, 0] = node_DOF[i].flatten()
+
+K_global = ds.global_stiffness_mat(members, node_cordinates, member_localz)
+
+displacements, reaction_forces, K_reduced = ds.solve_for_displacements_and_reactions(K_global, F_global, node_DOF)
+
 
 def test_global_stiffness_mat():
-    # Define test inputs
-    members = np.matrix([[500, 0.3, np.pi*0.5**2, (np.pi*0.5**4)/4, (np.pi*0.5**4)/4, (np.pi*0.5**4)/2, (np.pi*0.5**4)/2, 1, 5]])
-    node_cordinates = np.matrix([[0,0,0],[10,0,0]])
-    member_localz = np.array([0], dtype=object)
     
     # Run function
-    K_global = global_stiffness_mat(members, node_cordinates, member_localz)
+    K_global = ds.global_stiffness_mat(members, node_cordinates, member_localz)
     
     # Assert conditions
-    assert K_global.shape == (12, 12), "Global stiffness matrix size incorrect"
+    assert K_global.shape == (48, 48), "Global stiffness matrix size incorrect"
 
 def test_solve_for_displacements_and_reactions():
-    K_global = np.eye(12)  # Identity for test
-    F_global = np.ones((12,1))
-    node_DOF = np.ones((2,6))
     
-    displacements, reaction_forces, K_reduced = solve_for_displacements_and_reactions(K_global, F_global, node_DOF)
     
-    assert displacements.shape == (12,1), "Displacement vector size incorrect"
-    assert reaction_forces.shape == (12,1), "Reaction forces vector size incorrect"
+    displacements, reaction_forces, K_reduced = ds.solve_for_displacements_and_reactions(K_global, F_global, node_DOF)
+    
+    assert displacements.shape == (48,1), "Displacement vector size incorrect"
+    assert reaction_forces.shape == (24,1), "Reaction forces vector size incorrect"
+    assert K_reduced.shape == (24,24), "K_reduced mat size incorrect"
+
+
 
 def test_compute_internal_forces():
-    members = np.matrix([[500, 0.3, np.pi*0.5**2, (np.pi*0.5**4)/4, (np.pi*0.5**4)/4, (np.pi*0.5**4)/2, (np.pi*0.5**4)/2, 1, 5]])
-    node_cordinates = np.matrix([[0,0,0],[10,0,0]])
-    member_localz = np.array([0], dtype=object)
-    displacements = np.zeros((12,1))
     
-    internal_forces = compute_internal_forces(members, node_cordinates, member_localz, displacements)
+    internal_forces = ds.compute_internal_forces(members, node_cordinates, member_localz, displacements)
     
     assert len(internal_forces) == len(members), "Internal forces computation incorrect"
 
+
 def test_global_3D_stiffness_mat():
-    members = np.matrix([[500, 0.3, np.pi*0.5**2, (np.pi*0.5**4)/4, (np.pi*0.5**4)/4, (np.pi*0.5**4)/2, (np.pi*0.5**4)/2, 1, 5]])
-    node_cordinates = np.matrix([[0,0,0],[10,0,0]])
-    member_localz = np.array([0], dtype=object)
-    internal_forces = np.zeros(len(members))
     
-    Kg_global = global_3D_stiffness_mat(members, node_cordinates, member_localz, internal_forces)
+    internal_forces = np.array(ds.compute_internal_forces(members, node_cordinates, member_localz, displacements))
+    Kg_global = ds.global_3D_stiffness_mat(members, node_cordinates, member_localz, internal_forces)
     
-    assert Kg_global.shape == (12, 12), "Geometric stiffness matrix size incorrect"
+    assert Kg_global.shape == (48, 48), "Geometric stiffness matrix size incorrect"
+
+
 
 def test_solve_for_Kg_global_ff():
-    Kg_global = np.eye(12)
-    F_global = np.ones((12,1))
-    node_DOF = np.ones((2,6))
+    internal_forces = np.array(ds.compute_internal_forces(members, node_cordinates, member_localz, displacements))
+    Kg_global = ds.global_3D_stiffness_mat(members, node_cordinates, member_localz, internal_forces)
     
-    Kg_reduced = solve_for_Kg_global_ff(Kg_global, F_global, node_DOF)
+    Kg_reduced = ds.solve_for_Kg_global_ff(Kg_global, F_global, node_DOF)
     
-    assert Kg_reduced.shape[0] <= 12, "Reduced geometric stiffness matrix size incorrect"
+    assert Kg_reduced.shape == (24,24), "Reduced geometric stiffness matrix size incorrect"
 
 def test_solve_gen_eig():
-    K_global = np.eye(12)
-    Kg_global = np.eye(12) * 2
+    K_reduced = np.eye(12)
+    Kg_reduced = np.eye(12)
     
-    eigenvalues, eigenvectors = solve_gen_eig(K_global, Kg_global)
+    eigenvalues, eigenvectors = ds.solve_gen_eig(K_reduced, Kg_reduced)
     
-    assert len(eigenvalues) > 0, "Eigenvalue computation failed"
-    assert eigenvectors.shape[0] == 12, "Eigenvector size incorrect"
-    assert min(eigenvalues) > 0, "Critical force calculation incorrect"
+    assert eigenvalues[1] == (-1+0j), "Eigenvalue computation failed"
+    assert eigenvectors.shape == (12,12), "Eigenvector size incorrect"
+    
